2bbe45864f0251c23bf099a18ba30524271ea277
ashok-gitrepos <ashok.chirumamilla@yahoo.com>
2022-11-17 23:56:07 -0600

    Adding dumbell controllers

--- controllers/dumbell.js
+++ controllers/dumbell.js
@@ -1,136 +1,154 @@
-var dumbell = require('../models/dumbell');
-// List of all dumbells
-exports.dumbell_list = function(req, res) {
-    res.send('NOT IMPLEMENTED: dumbell list');
-};
-
-
-// for a specific dumbell.
-// exports.dumbell_detail = function(req, res) {
-//     res.send('NOT IMPLEMENTED: dumbell detail: ' + req.params.id);
-// };
-
-exports.dumbell_detail = async function(req, res) {
-    console.log("detail"  + req.params.id)
-    try {
-        result = await dumbell.findById( req.params.id)
-        res.send(result)
-    } catch (error) {
-        res.status(500)
-        res.send(`{"error": document for id ${req.params.id} not found`);
-    }
-};
-
-// Handle dumbell create on POST.
-exports.dumbell_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new dumbell();
-    // We are looking for a body, since POST does not have query parameters.
-// Even though bodies can be in many different formats, we will be picky
-// and require that it be a json object
-// {"dumbell_type":"goat", "cost":12, "size":"large"}
+var dumbell = require('../models/dumbell'); 
+ 
+// List of all dumbell 
+exports.dumbell_list = async function(req, res) { 
+    try{ 
+        thedumbells = await dumbell.find(); 
+        res.send(thedumbells); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+ // VIEWS 
+// Handle a show all view 
+exports.dumbell_view_all_Page = async function(req, res) { 
+    try{ 
+        thedumbells = await dumbell.find(); 
+        res.render('dumbell', { title: 'dumbell Search Results', results: thedumbells }); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
+// Handle dumbell create on POST. 
+exports.dumbell_create_post = async function(req, res) { 
+    console.log(req.body) 
+    let document = new dumbell(); 
+    // We are looking for a body, since POST does not have query parameters. 
+    // Even though bodies can be in many different formats, we will be picky 
+    // and require that it be a json object 
+    // {"Earp_Name":"Applepods", "Earp_Type":"Medium", "Earp_Size":3.5} 
     document.Dumbell_brand = req.body.Dumbell_brand;
     document.Dumbell_material = req.body.Dumbell_material;
     document.Dumbell_weight = req.body.Dumbell_weight;
-    
-    try{
-        let result = await document.save();
-        res.send(result);
-    }
-    catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }  
-};
+   
 
-exports.dumbell_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: dumbell create POST');
-};
+    try{ 
+        let result = await document.save(); 
+        res.send(result); 
+    } 
+    catch(err){ 
+        res.status(500); 
+        res.send(`{"error": ${err}}`); 
+    }   
+}; 
 
 
 
-// Handle dumbell delete form on DELETE.
-exports.dumbell_delete = function(req, res) {
-    res.send('NOT IMPLEMENTED: dumbell delete DELETE ' + req.params.id);
-};
+// for a specific dumbell. 
+exports.dumbell_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await dumbell.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 
-// Handle dumbell update form on PUT.
 
-// exports.dumbell_update_put = function(req, res) {
-//     res.send('NOT IMPLEMENTED: dumbell update PUT' + req.params.id);
-// };
 
-exports.dumbell_update_put = async function(req, res) {
-    console.log(`update on id ${req.params.id} with body
-${JSON.stringify(req.body)}`)
-    try {
-        let toUpdate = await dumbell.findById( req.params.id)
-        // Do updates of properties
+// Handle dumbell update form on PUT. 
+exports.dumbell_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await dumbell.findById( req.params.id) 
+        // Do updates of properties 
         if(req.body.Dumbell_brand)
                toUpdate.Dumbell_brand = req.body.Dumbell_brand;
         if(req.body.Dumbell_material) toUpdate.Dumbell_material = req.body.Dumbell_material;
         if(req.body.Dumbell_weight) toUpdate.Dumbell_weight = req.body.Dumbell_weight;
-        let result = await toUpdate.save();
-        console.log("Sucess " + result)
-        res.send(result)
-    } catch (err) {
-        res.status(500)
-        res.send(`{"error": ${err}: Update for id ${req.params.id}
-failed`);
-    }
-};
+        
+
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
+}; 
+  
 
 
-exports.dumbell_list = async function(req, res) {
-    try{
-        thedumbells = await dumbell.find();
-        res.send(thedumbells);
-    }
-    catch(err){
-        res.status(500);
-        res.send(`{"error": ${err}}`);
-    }  
-};
+// Handle dumbell delete on DELETE. 
+exports.dumbell_delete = async function(req, res) { 
+    console.log("delete "  + req.params.id) 
+    try { 
+        result = await dumbell.findByIdAndDelete( req.params.id) 
+        console.log("Removed " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": Error deleting ${err}}`); 
+    } 
+}; 
 
+// Handle a show one view with id specified by query 
+exports.dumbell_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await dumbell.findById( req.query.id) 
+        res.render('dumbelldetail',  { title: 'dumbell Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
 
+// Handle building the view for creating a dumbell. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.dumbell_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('dumbellcreate', { title: 'dumbell Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
 
-// VIEWS
-// Handle a show all view
-exports.dumbell_view_all_Page = async function(req, res) {
-    try{
-    thedumbells = await dumbell.find();
-    res.render('dumbells', { title: 'dumbell Search Results', results: thedumbells });
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
+// Handle building the view for updating a dumbell. 
+// query provides the id 
+exports.dumbell_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await dumbell.findById(req.query.id) 
+        res.render('dumbellupdate', { title: 'dumbell Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
 
-
-    // for a specific dumbell.
-// exports.dumbell_detail = async function(req, res) {
-//     console.log("detail" + req.params.id)
-//     try {
-//     result = await dumbell.findById( req.params.id)
-//     res.send(result)
-//     } catch (error) {
-//     res.status(500)
-//     res.send(`{"error": document for id ${req.params.id} not found`);
-//     }
-//     };
-
-
-
-// for a specific dumbell.
-exports.dumbell_detail = async function(req, res) {
-    console.log("detail"  + req.params.id)
-    try {
-        result = await dumbell.findById( req.params.id)
-        res.send(result)
-    } catch (error) {
-        res.status(500)
-        res.send(`{"error": document for id ${req.params.id} not found`);
-    }
-};
-//Handle dumbell update form on PUT.
+// Handle a delete one view with id from query 
+exports.dumbell_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await dumbell.findById(req.query.id) 
+        res.render('dumbelldelete', { title: 'dumbell Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/dumbell.js
+++ routes/dumbell.js
@@ -1,14 +1,28 @@
 var express = require('express');
-const dumbell_controlers= require('../controllers/dumbell');
+var dumbell_controller = require('../controllers/dumbell');
 var router = express.Router();
 
-/* GET home page. */
+// /* GET home page. */
 // router.get('/', function(req, res, next) {
-//   //res.render('dumbell', { title: 'Search results dumbell' });
-//   router.get('/', dumbell_controlers.dumbell_view_all_Page );
+//   res.render('dumbell', { title: 'Search Results for dumbell Class' });
 // });
 
+/* GET dumbell */ 
+router.get('/', dumbell_controller.dumbell_view_all_Page ); 
 
+// GET request for one dumbell. 
+router.get('/dumbells/:id', dumbell_controller.dumbell_detail);
 
-router.get('/', dumbell_controlers.dumbell_view_all_Page );
+/* GET detail dumbell page */ 
+router.get('/detail', dumbell_controller.dumbell_view_one_Page); 
+
+/* GET create dumbell page */ 
+router.get('/create', dumbell_controller.dumbell_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', dumbell_controller.dumbell_update_Page);
+
+/* GET delete dumbell page */ 
+router.get('/delete', dumbell_controller.dumbell_delete_Page); 
+
 module.exports = router;
